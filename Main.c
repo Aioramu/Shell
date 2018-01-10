fb9796e3326a76710b46ba142593918ee3069c21
Aioramu <Aioramu@users.noreply.github.com>
2018-01-10 23:22:12 +0300

    SH

---
+++ Main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include<sys/types.h>
+#include <unistd.h>
+#include<string.h>
+int main(void){
+    int st;
+    pid_t pid;
+
+for(;;)
+{
+    printf("enter the command: ");
+if(fgets(command,sizeof(command),stdin)==NULL){
+    printf("\n");
+    return 0;
+}
+
+command[strlen(command)-1]='\0';
+if (pid=fork()==0)
+if(command != NULL){
+execlp(command,command,0);
+continue;
+}
+while(wait(&st)!=pid)
+continue;
+ 
+printf("\n");
+}
+}
+
