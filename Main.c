f5ccf522cee081921ed22eb82596110906fee45c
Aioramu <31703899+Aioramu@users.noreply.github.com>
2018-01-11 15:18:28 +0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,30 +1,55 @@
 #include <stdio.h>
-#include <sys/wait.h>
-#include<sys/types.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dirent.h>
 #include <unistd.h>
-#include<string.h>
-int main(void){
-    char command[BUFSIZ];
-    int st;
-    pid_t pid;
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-for(;;)
+int main()
 {
-    printf("enter the command: ");
-if(fgets(command,sizeof(command),stdin)==NULL){
-    printf("\n");
-    return 0;
-}
+  while (1)
+  {
+    char cmd[255];
+    char **arguments = (char**) malloc(sizeof(char*));
+    int i = 1;
+    int ac = 0;
+    int c = 0;
+    char program[255];
+    signal(SIGINT,SIG_IGN);
+    printf ("Enter the command> ");
+    while ((c != '\n' && c != EOF) && (scanf("%s", cmd)))
+    {
+      /*printf ("%s\n", cmd);*/
+      if (!ac)
+      {
+        ac = 1;
+        *arguments = cmd;
+        strcpy(program, cmd);
+      } else
+      {
+        *(arguments + i) = cmd;
+        i++;
+      }
+      c = getchar();
+    }
+    if (c == EOF)
+    {
+      printf ("Termination!\n");
+      exit(0);
+    }
+    *(arguments + i) = NULL;
+    pid_t pid = fork();
+    if(pid==0)
+    {
 
-command[strlen(command)-1]='\0';
-if (pid=fork()==0)
-if(command != NULL){
-execlp(command,command,0);
-continue;
-}
-while(wait(&st)!=pid)
-continue;
- 
-printf("\n");
-}
+     if(execvp(program, arguments)!=0)
+      {
+      }
+      exit(1);
+    } 
+    int k;
+    wait(&k);
+  }
 }
